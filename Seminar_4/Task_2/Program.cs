843214b4fb741965623a34ab4e1aca0909f9e1c7
Olga <olga.krov@gmail.com>
2023-02-25 15:09:59 +0300

    tasks for seminar4

---
+++ Seminar_4/Task_2/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 27: Напишите программу, которая принимает на 
+// вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int Sum(int number){
+    
+    int counter = Convert.ToString(number).Length;
+    int a = 0;
+    int result = 0;
+
+    for (int i = 0; i < counter; i++){
+      a = number - number % 10;
+      result = result + (number - a);
+      number = number / 10;
+    }
+   return result;
+  }
+
+
+int n = ReadInt("Введите число: ");
+System.Console.WriteLine($"Сумма цифр в числе {n} = {Sum(n)}");
---
+++ Seminar_4/Task_3/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая задаёт массив
+// из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int[] GenerateArray(int size, int leftRange, int rightRange)
+{
+    int[] array = new int[size];
+    Random rand = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rand.Next(leftRange, rightRange + 1);
+    }
+    
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    System.Console.WriteLine("[" + string.Join(", ", array) + "]");
+}
+
+var myArray = GenerateArray(8, 0, 10);
+PrintArray(myArray);
