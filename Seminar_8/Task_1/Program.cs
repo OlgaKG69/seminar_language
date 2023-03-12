8dc07884c705829e559520411cb3c1e2a64aada2
Olga <olga.krov@gmail.com>
2023-03-12 11:46:50 +0300

    tasks for seminar8

---
+++ Seminar_8/Task_1/Program.cs
@@ -0,0 +1,69 @@
+﻿// Задача 54: Задайте двумерный массив. 
+// Напишите программу, которая упорядочит
+// по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+// НАДО:
+// 1. Функция заполнения массива рандомно числами от 1 до 9
+// 2. Функция сортировки элементов в строке двумерного массива по убыванию
+// 3. Функция вывода двумерного массива
+
+int[,] order = new int[3, 4];
+
+// 1
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+// 2
+void Sort(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int l = 0; l < array.GetLength(1) - 1; l++)
+            {
+                if (array[i, l] < array[i, l + 1])
+                {
+                    int coup = array[i, l + 1];
+                    array[i, l + 1] = array[i, l];
+                    array[i, l] = coup;
+                }
+            }
+        }
+    }
+}
+
+// 3
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+FillArray(order);
+PrintArray(order);
+Sort(order);
+Console.WriteLine();
+PrintArray(order);
---
+++ Seminar_8/Task_2/Program.cs
@@ -0,0 +1,78 @@
+﻿// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+// НАДО:
+// 1. Создать две матрицы
+// 2. Создать финальную матрицу, заполненную произведением двух предыдущих
+// 3. Функция запроса данных от пользователя
+// 4. Функция заполнения матриц
+// 5. Функция вывода матриц
+// 
+
+int m = ReadInt("Введите количество строк: ");
+int n = ReadInt("Введите количество столбцов: ");
+int[,] matrix = new int[m, n];
+int[,] secondMatrix = new int[m, n];
+int[,] finallyMatrix = new int[m, n];
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void FillMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+FillMatrix(matrix);
+PrintMatrix(matrix);
+Console.WriteLine();
+FillMatrix(secondMatrix);
+PrintMatrix(secondMatrix);
+Console.WriteLine();
+
+if (matrix.GetLength(0) != secondMatrix.GetLength(1))
+{
+    Console.WriteLine(" Операция невозможна ");
+    return;
+}
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < secondMatrix.GetLength(1); j++)
+    {
+        finallyMatrix[i, j] = 0;
+        for (int l = 0; l < matrix.GetLength(1); l++)
+        {
+            finallyMatrix[i, j] += matrix[i, l] * secondMatrix[l, j];
+        }
+    }
+}
+
+PrintMatrix(finallyMatrix);
+
+
