632c6f035c8edc1f9e34242ba0581ebbbe5f55fa
Olga <olga.krov@gmail.com>
2023-03-12 14:07:38 +0300

    tasks for seminar9

---
+++ Seminar_9/Task_1/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задайте значения M и N. Напишите программу,
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+// НАДО:
+// 1. Запросить у пользователя числа M и N
+// 2. Функция суммы натуальных чисел от M до N
+// 3. Рекурсия функции
+
+int Digit(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int SumMN(int m, int n)
+{
+    int result = m;
+    if (m == n)
+        return 0;
+    else
+    {
+        m++;
+        result = m + SumMN(m, n);
+        return result;
+    }
+}
+
+void MN(int m, int n)
+{
+    Console.Write(SumMN(m - 1, n));
+}
+
+int m = Digit("Введите число M: ");
+int n = Digit("Введите число N: ");
+MN(m, n);
---
+++ Seminar_9/Task_2/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии.
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+//          = n + 1                     m = 0
+// A(m, n)  = A (m - 1, 1)              m > 0, n = 0
+//          = A (m - 1, A(m, n - 1))    m > 0, n > 0
+
+int Number(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void Akkerman(int m, int n)
+{
+    Console.Write(AkkFunction(m, n));
+}
+
+int AkkFunction(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0 && m > 0)
+    {
+        return AkkFunction(m - 1, 1);
+    }
+    else
+    {
+        return (AkkFunction(m - 1, AkkFunction(m, n - 1)));
+    }
+}
+
+int m = Number("Введите число m: ");
+int n = Number("Введите число n: ");
+Akkerman(m, n);
